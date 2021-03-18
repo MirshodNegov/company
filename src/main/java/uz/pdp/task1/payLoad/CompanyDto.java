01c5fdd40fdb6f7e695b5a28d6962c51c090219a
MirshodNegov <mirshodnegov@mail.ru>
2021-03-18 15:26:00 +0500

    secondCommit

---
+++ src/main/java/uz/pdp/task1/payLoad/CompanyDto.java
@@ -0,0 +1,20 @@
+package uz.pdp.task1.payLoad;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.pdp.task1.entity.Address;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CompanyDto {
+    @NotNull(message = "Corp name can't be NULL")
+    private String corpName;
+    @NotNull(message = "Director name can't be NULL")
+    private Integer directorName;
+    @NotNull(message = "Address can't be NULL")
+    private Address address;    
+}
